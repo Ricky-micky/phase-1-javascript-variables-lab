4e8a7d19d1238818510333058a44a20df4f33c83
Ricky-micky <erick.tembo@student.moringaschool.com>
2024-09-24 14:38:31 +0300

    added text

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file!
+const companyName = "Scuber";
+//let companyName = 'Scuber';
+const CompanyName="scuder";
+let mostProfitableNeighborhood ="Chelsea";
+let companyCeo="Susan Smith"
